c32c596792daed933c9e7a313ca5945ceeebc352
santizo-z <santizozamoradavid@gmail.com>
2023-05-16 08:49:58 -0600

    .

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const display = document.getElementById('display');
 const historyContainer = document.getElementById('history');
 const buttons = [
-  ['AC', 'C', '/', 'X'],
+  ['AC', 'C', '/', '*'],
   ['9', '8', '7', '-'],
   ['6', '5', '4', '+'],
   ['3', '2', '1', '0'],
